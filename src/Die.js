0b10fb4e9b4cd65fea04665c1e9b21af5bdff837
Berfin Altunbey <baltunbey98@gmail.com>
2022-12-30 01:24:36 +0300

    add media queries

--- src/Die.js
+++ src/Die.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Die({value, isHeld, holdDice}) {
   return (
     <div className={(isHeld ? 'is-held ' : ' ' ) + 'die-box'} onClick={holdDice}>
-<h2> {value}</h2>
+<h2 className='die-num'> {value}</h2>
 </div>
   )
 }
